f3cce82fc0e9f7006373234afc65f9342e8b0c4f
Jovan Stojanov <jovan_stojanov@live.com>
2019-02-14 09:29:43 +0100

    test

---
+++ src/main/java/maven/org/handson/App.java
@@ -0,0 +1,13 @@
+package maven.org.handson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
